d034bfa817255000307a540f43e773947d87e29f
Analee Maharaj <amaharaj@stedwards.edu>
2024-01-30 21:14:40 +0000

    initial part1_exercises

--- Unit2_Assembly_Basics/part1_exercises.asm
+++ Unit2_Assembly_Basics/part1_exercises.asm
@@ -22,8 +22,78 @@ segment .text
 asm_main:
         enter   0,0               ; setup routine
         pusha
+        ; dump_regs 1
+        ; call print_n1
+        ; dump_regs 2
 
-		; TODO: add code
+        ; mov eax, 0
+        ; mov ah, 18
+        ; mov al, 0BAH
+        ; dump_regs 1
+        ; mov ax, 4
+        ; dump_regs 2
+
+        ; mov eax, 3
+        ; mov ebx, 4
+        ; add eax, ebx
+
+        ; dump_regs 1
+
+        ; mov eax, 40
+        ; dump_regs 1
+        ; call print_n1
+        ; mov ebx, 2
+        ; dump_regs 2
+        ; call print_n1
+        ; add eax, ebx
+        ; dump_regs 3
+        ; call print_n1
+
+        ;mov eax, 34
+        ;sub eax,19 ;immediate operand
+        ;dump_regs 1
+
+        mov eax , 16
+        dump_regs 1
+        call print_nl
+
+        sub eax, 4
+        dump_regs 2
+        call print_nl
+
+        mov ebx,eax
+        dump_regs 3
+        call print_nl
+
+        inc ebx
+        dump_regs 4
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+        mov ebx, 4
+
+        add ebx, ebx ; 2*4=8(2a)
+        add ebx, ebx ; 2*8=16(4a)
+
+        mov ecx, ebx ;2*16 =32 (8a)
+
+        add ebx, ecx ;16+32 =48 (12a)
+
+        dump_regs 1
+
+
+
+
+
+
+     
+
+
+
+        
+
+        		; TODO: add code
 
         popa
         mov     eax, 0            ; return back to C
